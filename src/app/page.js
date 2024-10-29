d7d6256b10c77e4f5a675c583b20d8e84d85bed4
24HP0880 <24hp0880@G305-CC0073.local>
2024-10-29 15:46:08 +0800

    timer

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,84 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+import { useState,useEffect } from "react"
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+const Timer=()=>{
+
+    const [count, setCount] = useState(60)
+    const [going, setGoing] = useState(false)
+    const second = count % 60
+    const minute = Math.floor(count/60)
+
+
+    const formattedTime = (minute, second) =>{
+        if(second < 9){
+            return `0${second}`;
+        }else if(second > 9){
+            return second;
+        }else if(minute < 9){
+            return`0${minute}`
+        }else if(minute > 9){
+            return minute;
+        }
+        
+    }
+
+    const timer = ()=>{
+        setCount((prev) =>(prev > 0? prev - 1: prev - 0))
+    }
+
+    const start=()=>{
+        setGoing(true)
+    }
+
+    const end=()=>{
+        setGoing(false)
+    }
+    
+    const add=()=>{
+        setCount((prev) => prev + 15)
+    }
+
+    const reset=()=>{
+        setCount(60)
+        setGoing(false)
+    }
+
+
+
+    useEffect(()=>{
+        const myInterval = setInterval(() => timer(),1000)
+        if(going){
+            myInterval
+        }else if(!going){
+            clearInterval(myInterval)
+        }
+        return ()=>clearInterval(myInterval);
+    },[going])
+
+    return(
+        <div className="clock_Background">
+            <div className="background-Image">
+                <div>
+                    <div className="time"><span className="minute">{formattedTime(minute)}</span>:<span>{formattedTime(second)}</span></div>
+                </div>
+                <div className="actionBtns">
+                    <div>
+                        <button className="btns" onClick={() => add()}>Add +15s</button>
+                    </div>
+                    <div>
+                        <button className="btns" onClick={() => start()}>Start</button>
+                    </div>
+                    <div>
+                        <button className="btns" onClick={() => end()}>End</button>
+                    </div>
+                    <div>
+                        <button className="btns" onClick={()=> reset()}>Reset</button>
+                    </div>
+                </div>
+            </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    )
 }
+
+export default Timer;
